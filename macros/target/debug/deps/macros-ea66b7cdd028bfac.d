b36967596ab638b1fd08934fe609055088313e30
Toby Martin <toby.martin@me.com>
2026-02-14 10:00:15 +1100

    Removed target

--- macros/target/debug/deps/macros-ea66b7cdd028bfac.d
+++
@@ -1,5 +0,0 @@
-/home/toby/dev/librarian/macros/target/debug/deps/macros-ea66b7cdd028bfac.d: src/lib.rs
-
-/home/toby/dev/librarian/macros/target/debug/deps/libmacros-ea66b7cdd028bfac.rmeta: src/lib.rs
-
-src/lib.rs:
